1c27efc2b6a6c6d913b5f479256a1700691aa22e
bluemin <zeem_m2@naver.com>
2024-12-24 16:10:59 +0900

    style: tailwind 셋팅
    - 의존성 재배치
    - 로컬 폰트 셋팅
    - primary 색상 추가 및 gray 색상 커스텀 변경
    - postcss.config.js 파일 추가

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      gray: {
+        100: '#EFF2F6',
+        200: '#EEEEEE',
+        300: '#ECECEC',
+        400: '#CACACA',
+        500: '#A1A1A1',
+        600: '#9E9E9E',
+        700: '#656565',
+        800: '#535353',
+        900: '#505050',
+      },
+    },
+    extend: {
+      fontFamily: {
+        inter: ['Inter'],
+        nanumGothic: ['NanumGothic'],
+      },
+      colors: {
+        primary: '#ADE145',
+      },
+    },
+  },
+  plugins: [],
+};
