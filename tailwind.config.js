3ea9775ab432064d108e182a8b1d38c2d0cf24e0
bluemin <zeem_m2@naver.com>
2024-12-24 17:25:05 +0900

    design: Header UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,6 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      gray: {
-        100: '#EFF2F6',
-        200: '#EEEEEE',
-        300: '#ECECEC',
-        400: '#CACACA',
-        500: '#A1A1A1',
-        600: '#9E9E9E',
-        700: '#656565',
-        800: '#535353',
-        900: '#505050',
-      },
-    },
     extend: {
       fontFamily: {
         inter: ['Inter'],
@@ -21,6 +8,17 @@ export default {
       },
       colors: {
         primary: '#ADE145',
+        gray: {
+          100: '#EFF2F6',
+          200: '#EEEEEE',
+          300: '#ECECEC',
+          400: '#CACACA',
+          500: '#A1A1A1',
+          600: '#9E9E9E',
+          700: '#656565',
+          800: '#535353',
+          900: '#505050',
+        },
       },
     },
   },
