a9fb5c3d0476add8d43f782e39a9de82726702c2
bluemin <zeem_m2@naver.com>
2024-12-25 16:31:37 +0900

    comment: 해야할 일을 나타내기 위해 TODO로 변경

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,7 +4,7 @@ import LoginPage from '../pages/LoginPage';
 const Router = () => {
   return (
     <Routes>
-      {/* 메인 페이지 - 모집글 조회 페이지 */}
+      {/* TODO: 메인 페이지 - 모집글 조회 페이지 추가하기 */}
       {/* <Route path="/" element={} /> */}
 
       {/* 로그인 페이지 */}
