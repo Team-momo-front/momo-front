c24cb84e8a5cf26c04fedf710e1afbae9d2844ea
bluemin <zeem_m2@naver.com>
2024-12-26 15:42:33 +0900

    feat: 회원가입 페이지 UI 구현
    - 회원가입 폼, 입력 필드 및 제출 버튼 추가
    - 에러 메시지 처리 구현
    - 에러 색상 및 메시지 처리 위한 테일윈드 error color 추가

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,19 +2,30 @@ interface InputProps {
   type: string;
   name: string;
   id?: string;
-  // value: string;
-  // onChange: React.ChangeEventHandler<HTMLInputElement>;
+  value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
   placeholder: string;
   required: boolean;
   className?: string;
 }
 
-const Input: React.FC<InputProps> = ({ type, name, id, placeholder, required = true, className = '' }) => {
+const Input: React.FC<InputProps> = ({
+  type,
+  name,
+  id,
+  value,
+  onChange,
+  placeholder,
+  required = true,
+  className = '',
+}) => {
   return (
     <input
       id={id}
       type={type}
       name={name}
+      value={value}
+      onChange={onChange}
       placeholder={placeholder}
       required={required}
       className={`w-full h-[50px] mb-4 p-4 shadow-[0_4px_4px_1px_rgba(0,0,0,0.25)] rounded-md font-bold text-sm placeholder-gray-500 outline-none ${className}`}
--- src/components/Join/JoinForm.tsx
+++ src/components/Join/JoinForm.tsx
@@ -1,22 +1,65 @@
+import { useState } from 'react';
 import Input from '../Input';
 
 const JoinForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const [emailError, setEmailError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [passwordConfirmError, setPasswordConfirmError] = useState<string | null>(null);
+  const [nicknameError, setNicknameError] = useState<string | null>(null);
+  const [phoneNumberError, setPhoneNumberError] = useState<string | null>(null);
+
+  const handleJoinSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setEmailError(null);
+    setPasswordError(null);
+    setPasswordConfirmError(null);
+    setNicknameError(null);
+    setPhoneNumberError(null);
+
+    // TODO: validation code here
+    // TODO: API 요청, joinSubmit logic
+  };
+
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="max-w-[538px] flex gap-x-6">
-        <form
-          className="w-[320px]"
-          // TODO: onSubmit={handleJoinSubmit}
-        >
+        <form className="w-[320px]" onSubmit={handleJoinSubmit}>
           <label htmlFor="email" className="font-bold text-lg mb-1">
             이메일
           </label>
-          <Input id="email" type="email" name="email" placeholder="이메일을 입력해주세요." required />
+          <Input
+            id="email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="이메일을 입력해주세요."
+            required
+            className={`mb-2 ${emailError && 'border-error border-2'}`}
+          />
+          <p className="mb-2 font-bold text-[12px] text-error">{emailError}</p>
 
           <label htmlFor="password" className="font-bold text-lg mt-[30px] mb-1">
             비밀번호
           </label>
-          <Input id="password" type="password" name="password" placeholder="비밀번호를 입력해주세요." required />
+          <Input
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="비밀번호를 입력해주세요."
+            required
+            className={`mb-2 ${passwordError && 'border-error border-2'}`}
+          />
+          <p className="mb-2 font-bold text-[12px] text-error">{passwordError}</p>
 
           <label htmlFor="passwordConfirm" className="font-bold text-lg mt-[30px] mb-1">
             비밀번호 확인
@@ -25,34 +68,60 @@ const JoinForm = () => {
             id="passwordConfirm"
             type="password"
             name="passwordConfirm"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
             placeholder="비밀번호를 다시 한 번 입력해주세요."
             required
+            className={`mb-2 ${passwordConfirmError && 'border-error border-2'}`}
           />
+          <p className="mb-2 font-bold text-[12px] text-error">{passwordConfirmError}</p>
 
           <label htmlFor="nickname" className="font-bold text-lg mt-[30px] mb-1">
             닉네임
           </label>
-          <Input id="nickname" type="text" name="nickname" placeholder="닉네임을 입력해주세요." required />
+          <Input
+            id="nickname"
+            type="text"
+            name="nickname"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+            placeholder="닉네임을 입력해주세요."
+            required
+            className={`mb-2 ${nicknameError && 'border-error border-2'}`}
+          />
+          <p className="mb-2 font-bold text-[12px] text-error">{nicknameError}</p>
 
-          <label htmlFor="phonenumber" className="font-bold text-lg mt-[30px] mb-1">
+          <label htmlFor="phone-number" className="font-bold text-lg mt-[30px] mb-1">
             휴대폰 번호
           </label>
-          <Input id="phonenumber" type="tel" name="tel" placeholder="010-1234-5678" required />
+          <Input
+            id="phone-number"
+            type="tel"
+            name="tel"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder="010-1234-5678"
+            required
+            className={`mb-2 ${phoneNumberError && 'border-error border-2'}`}
+          />
+          <p className="mb-2 font-bold text-[12px] text-error">{phoneNumberError}</p>
 
-          <div className="w-full flex justify-center items-center">
-            <button
-              type="submit"
-              className="w-[140px] h-[30px] mt-[30px] items-center bg-primary rounded-md font-bold text-white text-[13.5px]">
-              다음 단계로 넘어가기
-            </button>
-          </div>
+          {!emailError && !passwordError && !passwordConfirmError && !nicknameError && !phoneNumberError && (
+            <div className="w-full flex justify-center items-center">
+              <button
+                type="submit"
+                className="w-[140px] h-[30px] mt-[30px] items-center bg-primary rounded-md font-bold text-white text-[13.5px]">
+                다음 단계로 넘어가기
+              </button>
+            </div>
+          )}
         </form>
 
         <button
           type="button"
           // TODO: onClick={handleEmailValidation}
           className="mt-10 w-[90px] h-[30px] items-center bg-primary rounded-md font-bold text-white text-[13.5px]">
-          인증하기
+          이메일 인증
         </button>
       </div>
     </div>
