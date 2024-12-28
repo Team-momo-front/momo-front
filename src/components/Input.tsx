6499806bdf2cb6444b0192668c1f7f5d43727b5e
bluemin <zeem_m2@naver.com>
2024-12-28 23:18:53 +0900

    🎨 design(styles): Input 컴포넌트에 Daisy UI input 스타일 적용
    - Daisy UI의 input 스타일을 Input 컴포넌트에 추가하여 통일된 디자인 제공

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -31,7 +31,7 @@ const Input: React.FC<InputProps> = ({
       onBlur={onBlur}
       placeholder={placeholder}
       required={required}
-      className={`w-full h-[50px] mb-4 p-4 shadow-[0_4px_4px_1px_rgba(0,0,0,0.25)] rounded-md font-bold text-sm placeholder-gray-500 outline-none ${className}`}
+      className={`input input-bordered w-full max-w-xs mb-4 p-4 font-bold text-sm placeholder-gray-500 focus:outline-none ${className}`}
     />
   );
 };
