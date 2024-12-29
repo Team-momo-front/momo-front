2f13e60190a1259187e5dfbafa3abdf9cab49de1
Dean <86055589+anony02@users.noreply.github.com>
2024-12-29 18:11:01 +0900

    헤더 로고 클릭 시 메인페이지로 이동

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,16 +4,21 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className="w-full h-[62px] border-b-[1px] border-gray-100 grid grid-cols-3 items-center">
-      <div></div>
+      <div />
       <div className="flex justify-center items-center gap-2">
-        <img src={logo} alt="logo" className="w-16" />
-        <span className="font-inter font-bold text-xl text-gray-600">momo</span>
+        <Link to="/" className="flex items-center gap-2">
+          <img src={logo} alt="logo" className="w-16" />
+          <span className="font-inter font-bold text-xl text-gray-600">
+            momo
+          </span>
+        </Link>
       </div>
-
       {/* buttons */}
       <div className="flex justify-end gap-4 mr-4">
         <Link to="/login">
-          <button className="w-16 h-8 shadow-md rounded-md font-inter hover:bg-primary hover:text-white">Log in</button>
+          <button className="w-16 h-8 shadow-md rounded-md font-inter hover:bg-primary hover:text-white">
+            Log in
+          </button>
         </Link>
         <Link to="/join">
           <button className="w-16 h-8 bg-gray-600 shadow-md rounded-md font-inter text-white hover:bg-primary">
