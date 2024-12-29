9cc21c22ef3cc9bc236692d24a30188e36b776ec
bluemin <zeem_m2@naver.com>
2024-12-29 19:50:18 +0900

    💄 style: 빈 태그를 닫힌 태그로 수정

--- src/components/Join/ResetPassword.tsx
+++ src/components/Join/ResetPassword.tsx
@@ -57,7 +57,7 @@ const ResetPassword = () => {
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
+              />
             </svg>
             <p className="w-full font-bold text-sm text-center">{success}</p>
             <div>
