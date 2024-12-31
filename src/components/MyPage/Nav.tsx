7e6bb1095dbb9cc53394edda47a3a13eae646aa6
bluemin <zeem_m2@naver.com>
2024-12-31 20:22:55 +0900

    🎨 style: hover 스타일링 수정

--- src/components/MyPage/Nav.tsx
+++ src/components/MyPage/Nav.tsx
@@ -8,42 +8,50 @@ const Nav = () => {
           마이페이지
         </span>
         <ul className="flex gap-6 mb-4">
-          <li className="font-bold text-[16px] hover:text-primary">
+          <li className="font-bold text-[16px]">
             <NavLink
               to="/mypage/my-info"
               className={({ isActive }) =>
-                isActive ? 'text-primary' : 'text-gray-500 focus:text-primary'
+                isActive
+                  ? 'text-primary'
+                  : 'text-gray-500 focus:text-primary hover:text-primary'
               }
             >
               내 정보
             </NavLink>
           </li>
           {/* TODO: 각각 라우터 생성하고 경로 설정 */}
-          <li className="font-bold text-[16px] hover:text-primary">
+          <li className="font-bold text-[16px]">
             <NavLink
               to="/"
               className={({ isActive }) =>
-                isActive ? 'text-primary' : 'text-gray-500 focus:text-primary'
+                isActive
+                  ? 'text-primary'
+                  : 'text-gray-500 focus:text-primary hover:text-primary'
               }
             >
               내 모임 관리
             </NavLink>
           </li>
-          <li className="font-bold text-[16px] hover:text-primary">
+          <li className="font-bold text-[16px]">
             <NavLink
               to="/mypage/change-password"
               className={({ isActive }) =>
-                isActive ? 'text-primary' : 'text-gray-500 focus:text-primary'
+                isActive
+                  ? 'text-primary'
+                  : 'text-gray-500 focus:text-primary hover:text-primary'
               }
             >
               비밀번호 변경
             </NavLink>
           </li>
-          <li className="font-bold text-[16px] hover:text-primary">
+          <li className="font-bold text-[16px]">
             <NavLink
               to="/mypage/account-delection"
               className={({ isActive }) =>
-                isActive ? 'text-primary' : 'text-gray-500 focus:text-primary'
+                isActive
+                  ? 'text-primary'
+                  : 'text-gray-500 focus:text-primary hover:text-primary'
               }
             >
               회원 탈퇴
