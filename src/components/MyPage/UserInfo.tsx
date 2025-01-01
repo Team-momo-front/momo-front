d044ed108da88b5552a119f9a52110835e3ce4bf
bluemin <zeem_m2@naver.com>
2025-01-01 14:59:04 +0900

    🔥 style: 불필요한 Fragment 제거

--- src/components/MyPage/UserInfo.tsx
+++ src/components/MyPage/UserInfo.tsx
@@ -55,109 +55,107 @@ const UserInfo: React.FC<UserInfoProps> = ({ isModified, isCanceled }) => {
   }, [nicknameError, mbtiError, phoneNumberError]);
 
   return (
-    <>
-      <div className="flex justify-between gap-10">
-        <div className="flex flex-col gap-6">
-          <InfoFormField
-            name="nickname"
-            label="닉네임"
-            type="text"
-            disabled={!isModified}
-            required
-            value={updatedUserData.nickname}
-            onChange={e => handleChange('nickname', e.target.value)}
-            onBlur={e => {
-              setNicknameError(validateNickname(e.target.value));
-            }}
-            isModified={isModified}
-            error={nicknameError}
-          />
+    <div className="flex justify-between gap-10">
+      <div className="flex flex-col gap-6">
+        <InfoFormField
+          name="nickname"
+          label="닉네임"
+          type="text"
+          disabled={!isModified}
+          required
+          value={updatedUserData.nickname}
+          onChange={e => handleChange('nickname', e.target.value)}
+          onBlur={e => {
+            setNicknameError(validateNickname(e.target.value));
+          }}
+          isModified={isModified}
+          error={nicknameError}
+        />
 
-          <InfoFormField
-            name="email"
-            label="이메일"
-            type="text"
-            placeholder={updatedUserData.email}
-            disabled
-            isModified={isModified}
-          />
+        <InfoFormField
+          name="email"
+          label="이메일"
+          type="text"
+          placeholder={updatedUserData.email}
+          disabled
+          isModified={isModified}
+        />
 
-          <InfoFormField
-            name="phoneNumber"
-            label="휴대폰 번호"
-            type="tel"
-            placeholder={updatedUserData.phoneNumber}
-            disabled={!isModified}
-            value={updatedUserData.phoneNumber}
-            onChange={e =>
-              handleChange('phoneNumber', formatPhoneNumber(e.target.value))
+        <InfoFormField
+          name="phoneNumber"
+          label="휴대폰 번호"
+          type="tel"
+          placeholder={updatedUserData.phoneNumber}
+          disabled={!isModified}
+          value={updatedUserData.phoneNumber}
+          onChange={e =>
+            handleChange('phoneNumber', formatPhoneNumber(e.target.value))
+          }
+          onBlur={e => {
+            if (!e.target.value) {
+              setPhoneNumberError('휴대폰 번호를 입력해주세요.');
+            } else {
+              setPhoneNumberError(validatePhoneNumber(e.target.value));
             }
-            onBlur={e => {
-              if (!e.target.value) {
-                setPhoneNumberError('휴대폰 번호를 입력해주세요.');
-              } else {
-                setPhoneNumberError(validatePhoneNumber(e.target.value));
+          }}
+          isModified={isModified}
+          error={phoneNumberError}
+        />
+
+        <div className="w-[320px]">
+          <label className="form-control w-full max-w-xs">
+            <div className="label p-0">
+              <span className="label-text font-bold text-lg mb-2">
+                자기소개
+              </span>
+            </div>
+            <textarea
+              value={updatedUserData.introduction}
+              className={
+                isModified
+                  ? 'textarea textarea-bordered w-full h-24 py-4 placeholder-gray-500 font-bold text-sm focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary resize-none'
+                  : 'textarea-custom-disabled textarea-bordered h-24 py-4 resize-none font-bold placeholder:font-bold'
               }
-            }}
-            isModified={isModified}
-            error={phoneNumberError}
-          />
-
-          <div className="w-[320px]">
-            <label className="form-control w-full max-w-xs">
-              <div className="label p-0">
-                <span className="label-text font-bold text-lg mb-2">
-                  자기소개
-                </span>
-              </div>
-              <textarea
-                value={updatedUserData.introduction}
-                className={
-                  isModified
-                    ? 'textarea textarea-bordered w-full h-24 py-4 placeholder-gray-500 font-bold text-sm focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary resize-none'
-                    : 'textarea-custom-disabled textarea-bordered h-24 py-4 resize-none font-bold placeholder:font-bold'
-                }
-                onChange={e => handleChange('introduction', e.target.value)}
-                disabled={!isModified}
-              ></textarea>
-            </label>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-6">
-          <InfoFormField
-            name="gender"
-            label="성별"
-            type="text"
-            placeholder={convertGenderToLabel(updatedUserData.gender)}
-            disabled
-            isModified={isModified}
-          />
-
-          <InfoFormField
-            name="birth"
-            label="생년 월일"
-            type="text"
-            placeholder={updatedUserData.birth}
-            disabled
-            isModified={isModified}
-          />
-
-          <InfoFormField
-            name="mbti"
-            label="MBTI"
-            type="text"
-            disabled={!isModified}
-            value={updatedUserData.mbti}
-            onChange={e => handleChange('mbti', e.target.value.toUpperCase())}
-            onBlur={e => validateMBTI(e.target.value)}
-            isModified={isModified}
-            maxLength={4}
-            error={mbtiError}
-          />
+              onChange={e => handleChange('introduction', e.target.value)}
+              disabled={!isModified}
+            ></textarea>
+          </label>
         </div>
       </div>
-    </>
+
+      <div className="flex flex-col gap-6">
+        <InfoFormField
+          name="gender"
+          label="성별"
+          type="text"
+          placeholder={convertGenderToLabel(updatedUserData.gender)}
+          disabled
+          isModified={isModified}
+        />
+
+        <InfoFormField
+          name="birth"
+          label="생년 월일"
+          type="text"
+          placeholder={updatedUserData.birth}
+          disabled
+          isModified={isModified}
+        />
+
+        <InfoFormField
+          name="mbti"
+          label="MBTI"
+          type="text"
+          disabled={!isModified}
+          value={updatedUserData.mbti}
+          onChange={e => handleChange('mbti', e.target.value.toUpperCase())}
+          onBlur={e => validateMBTI(e.target.value)}
+          isModified={isModified}
+          maxLength={4}
+          error={mbtiError}
+        />
+      </div>
+    </div>
   );
 };
 
