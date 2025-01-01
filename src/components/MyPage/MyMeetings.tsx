aa96dccaf4a7a38c547dcd82d4e5429698a6aa9d
bluemin <zeem_m2@naver.com>
2025-01-02 02:19:01 +0900

    🎉 feat(button): isActive 추가로 버튼에 따른 컴포넌트 렌더링 구현

--- src/components/MyPage/MyMeetings.tsx
+++ src/components/MyPage/MyMeetings.tsx
@@ -1,19 +1,30 @@
+import { useState } from 'react';
 import HostedMeetings from './HostedMeetings';
+import ParticipatedMeetings from './ParticipatedMeetings';
 
 const MyMeetings = () => {
+  const [isActive, setIsActive] = useState('hosted');
+
   return (
     <div className="w-full">
       <div className="w-[680px] m-auto flex flex-col items-center justify-center mt-[30px]">
         <div className="flex h-6 text-16px items-center">
-          <span className="text-[16px] cursor-pointer hover:font-bold transition-all">
+          <span
+            className="text-[16px] cursor-pointer hover:font-bold transition-all"
+            onClick={() => setIsActive('hosted')}
+          >
             주최한 모임
           </span>
           <div className="divider divider-horizontal"></div>
-          <span className="text-[16px] cursor-pointer hover:font-bold transition-all">
+          <span
+            className="text-[16px] cursor-pointer hover:font-bold transition-all"
+            onClick={() => setIsActive('participated')}
+          >
             신청한 모임
           </span>
         </div>
-        <HostedMeetings />
+        {isActive === 'hosted' && <HostedMeetings />}
+        {isActive === 'participated' && <ParticipatedMeetings />}
       </div>
     </div>
   );
