f2746c7b4d96dd71b2fd15e9ceb341b2c1122109
bluemin <zeem_m2@naver.com>
2025-01-16 21:04:15 +0900

    ♻️ refactor(config): clientId, redirectUri 환경변수로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
