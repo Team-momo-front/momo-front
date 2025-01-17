dfc61a5a2f43a435d9a2a500ae8c53171d46ed06
Dean <86055589+anony02@users.noreply.github.com>
2025-01-18 04:20:50 +0900

    ✨ Feat: 게시글 수정 API 호출 훅

---
+++ src/hooks/useEditMeeting.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { editMeeting } from '../api/meeting';
+import type {
+  CreateMeetingRequest,
+  CreateMeetingResponse,
+} from '../types/Meeting';
+
+const useEditMeeting = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, body }: { id: string; body: CreateMeetingRequest }) =>
+      editMeeting(id, body),
+    onSuccess: (updatedData, variables) =>
+      queryClient.setQueryData<CreateMeetingResponse>(
+        ['meetingDetail', variables.id],
+        updatedData
+      ),
+  });
+};
+
+export default useEditMeeting;
