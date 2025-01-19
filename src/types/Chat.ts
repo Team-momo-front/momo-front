c786c54e26e604f6c10b14ea5f2d6478d07b906b
bluemin <zeem_m2@naver.com>
2025-01-20 04:39:08 +0900

    🎉 feat(chat): 채팅 API 호출 함수 정의

--- src/types/Chat.ts
+++ src/types/Chat.ts
@@ -8,3 +8,29 @@ export interface Chat {
   unreadMessagesCount: number | null;
   readerId: string[];
 }
+
+export interface ChatRoomResponse {
+  roomId: number;
+  meetingId: number;
+  meetingThumbnail: string;
+  meetingTitle: string;
+  lastMessage: string;
+  hostId: number;
+  readerId: number[];
+  unreadMessagesCount: number | null;
+}
+
+export interface ChatHistoryResponse {
+  userId: number;
+  userNickname: string;
+  userProfileImageUrl: string;
+  message: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ChatParticipant {
+  userId: number;
+  nickname: string;
+  profileImageUrl: string;
+}
