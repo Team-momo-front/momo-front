b29db9904b32b3a2e410c450cb96518690131004
bluemin <zeem_m2@naver.com>
2025-01-20 11:16:05 +0900

    🔒 fix(auth): 401 오류 발생 시 액세스 토큰 재발급 요청 및 카카오 로그인 실행 트리거 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { refreshAccessToken } from './refreshAccessToken';
 
 const axiosInstance = axios.create();
 
@@ -20,14 +21,25 @@ axiosInstance.interceptors.response.use(
     // TODO : 공통적으로 처리할 일이 있으면 여기서 처리
     return response;
   },
-  error => {
+  async error => {
     // TODO : 401, 403 등의 에러 처리
     // if (error.response && error.response.status === 401) {
     // }
-    console.log(error);
     if (error.response && error.response.status === 403) {
       // 프로필 검증 오류시 로컬스토리지 'hasProfile'에 'false'값 저장
       localStorage.setItem('hasProfile', 'false');
+    }
+    if (error.response && error.response.status === 401) {
+      const newToken = await refreshAccessToken();
+      if (newToken) {
+        error.config.headers.Authorization = `Bearer ${newToken}`;
+        return axiosInstance(error.config);
+      }
+
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('loginType');
+      localStorage.removeItem('hasProfile');
+      localStorage.removeItem('userId');
     }
     return Promise.reject(error);
   }
