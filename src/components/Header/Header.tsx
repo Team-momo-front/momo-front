d847e288263d98b96dd6ff2c50131ed31cc41ca1
bluemin <zeem_m2@naver.com>
2025-01-22 18:21:07 +0900

    🐛 fix: 헤더 이미지 에러 처리 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -126,6 +126,10 @@ const Header = () => {
               src={profileImageUrl || '/image/default_profile_image.png'}
               alt="profile image"
               className="w-[30px] h-[30px] object-cover p-[1px] border border-gray-600 rounded-full cursor-pointer"
+              // 에러 처리 추가
+              onError={e => {
+                e.currentTarget.src = '/image/default_profile_image.png';
+              }}
             />
             <ul
               tabIndex={0}
