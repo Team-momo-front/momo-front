ac5cc27bc3b3aa2d8030a5219148acc1704abf3a
bluemin <zeem_m2@naver.com>
2025-02-01 01:19:39 +0900

    ✨ feat: hasApplied 구현

--- src/pages/ApplyMeetingPage.tsx
+++ src/pages/ApplyMeetingPage.tsx
@@ -1,10 +1,16 @@
 import DetailPageLayout from '../components/DetailPageLayout';
+import useGetMyParticipatedMeetings from '../hooks/useGetMyParticipatedMeetings';
 import useParticipateMeeting from '../hooks/useParticipateMeeting';
 import { Post } from '../types/Post';
 
 const ApplyMeetingPage = ({ meeting }: { meeting: Post }) => {
-  const hasApplied = false; // TODO: 참여신청한 목록 조회에서 반환하는 meetingId로 처리해야할 것 같습니다.
+  const { data } = useGetMyParticipatedMeetings({});
+  const appliedMeetingIds = data?.meetingIds;
+  let hasApplied = false;
+  if (appliedMeetingIds?.includes(meeting.id)) hasApplied = true;
+
   const { mutate: participateMeeting } = useParticipateMeeting(meeting.id);
+
   const handleClick = () => {};
 
   const handleParticipate = () => {
