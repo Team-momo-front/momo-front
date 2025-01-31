1b5ab09aec4dc20aac2c55022fba2e0af9daf195
bluemin <zeem_m2@naver.com>
2025-01-31 23:22:37 +0900

    ✨ feat: categoryValueToKey 함수 분리

--- src/hooks/useCreatePostForm.ts
+++ src/hooks/useCreatePostForm.ts
@@ -11,7 +11,7 @@ const useCreatePostForm = () => {
     address: '',
     meetingDateTime: '',
     maxCount: 0,
-    categories: [],
+    category: [],
     content: '',
   });
 
--- src/pages/CreatePostPage.tsx
+++ src/pages/CreatePostPage.tsx
@@ -11,23 +11,7 @@ import {
   getOneYearLaterDateTime,
 } from '../utils/getLocalDateTime';
 import LoadingSpinner from '../components/LoadingSpinner';
-
-enum CategoryList {
-  KOREAN = '한식',
-  WESTERN = '양식',
-  CHINESE = '중식',
-  JAPANESE = '일식',
-  DESSERT = '디저트',
-  OTHER = '기타',
-}
-
-type CategoryKey = keyof typeof CategoryList;
-
-const categoryValueToKey = Object.keys(CategoryList).reduce((acc, key) => {
-  const value = CategoryList[key as CategoryKey];
-  acc[value] = key as CategoryKey;
-  return acc;
-}, {} as { [key: string]: CategoryKey });
+import { categoryValueToKey } from '../utils/categoryValueToKey';
 
 const CreatePostPage = () => {
   const {
