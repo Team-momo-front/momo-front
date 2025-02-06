6271172fb8b197f1f0819e35a16e6e246e34d9e3
bluemin <zeem_m2@naver.com>
2025-02-06 22:02:33 +0900

    ♻️ refactor: refetch 대신 invalidateQueries 사용하도록 수정

--- src/hooks/useGetOutChatRoom.ts
+++ src/hooks/useGetOutChatRoom.ts
@@ -1,12 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { getOutChatRoom } from '../api/chatrooms';
 
 export const useGetOutChatRoom = () => {
+  const queryclient = useQueryClient();
   return useMutation({
     mutationFn: (roomId: number) => getOutChatRoom(roomId),
     onSuccess: () => {
       console.log('채팅방 나가기(뒤로가기)');
+      queryclient.invalidateQueries({ queryKey: ['get-chatroom-list'] });
     },
     onError: error => {
       if (axios.isAxiosError(error)) {
