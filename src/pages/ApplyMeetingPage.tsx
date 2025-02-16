73aaa4b05f345cb6d4078539caf8c84ddb3279b7
bluemin <zeem_m2@naver.com>
2025-02-16 17:02:32 +0900

    ♻️ refactor: 타입 오류 수정

--- src/pages/ApplyMeetingPage.tsx
+++ src/pages/ApplyMeetingPage.tsx
@@ -13,7 +13,7 @@ const ApplyMeetingPage = ({ meeting }: { meeting: Post }) => {
     !!appliedMeetingIds?.includes(meeting.id)
   );
 
-  let participatingId = 0;
+  let participatingId: number;
   if (participatingIds && appliedMeetingIds) {
     participatingId = participatingIds[appliedMeetingIds.indexOf(meeting.id)];
   }
